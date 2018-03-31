372d5982577ddd58fe500d4cc198e10ec0d2969c
OvidiuDobran <dobran_ovi@yahoo.com>
2018-03-31 03:30:23 +0300

    Formatted the EditText inputs and arranged the code

--- app/src/main/java/com/example/ovi/fixyourcity/MyRelativeLayout.java
+++ app/src/main/java/com/example/ovi/fixyourcity/MyRelativeLayout.java
@@ -7,12 +7,27 @@ import android.widget.RelativeLayout;
  * Created by Ovi on 3/30/2018.
  */
 
-public abstract class MyRelativeLayout extends RelativeLayout{
+public abstract class MyRelativeLayout extends RelativeLayout {
+    private MainActivity mainActivity;
+
     public MyRelativeLayout(Context context) {
         super(context);
+        if (context instanceof MainActivity) {
+            mainActivity = (MainActivity) context;
+        }
         createContent();
+        addBehaviours();
     }
 
     protected abstract void createContent();
 
+    protected abstract void addBehaviours();
+
+    public MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+    public void setMainActivity(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
 }
